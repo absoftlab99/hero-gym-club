5538e735b082cef5f0910829ad397b5ebd54f61b
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-09-29 13:08:37 +0600

    logo animation added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='container bg-dark rounded-3'>
-            <h1 className='display-5 fw-bolder p-4 text-warning'><i className='fa-solid fa-dumbbell'></i> Hero Club</h1>
+            <h1 className='display-5 fw-bolder p-4 text-warning'><i className='fa-solid fa-dumbbell fa-bounce'></i> Hero Club</h1>
         </div>
     );
 };
--- src/components/Times/Times.js
+++ src/components/Times/Times.js
@@ -7,12 +7,12 @@ const Times = () => {
             <h4 className='ff-poppins fw-bold text-light'>Exercise Details</h4>
             <div className="row bg-warning rounded-3 break mx-0">
                 <div className="col-7 time"><h5 className=''>Exercise time</h5></div>
-                <div className="col-5 times"><p className='text-muted'>200 seconds</p></div>
+                <div className="col-5 times"><p className='text-muted'><span id='exercise-time'>0</span> seconds</p></div>
             </div>
 
             <div className="row bg-warning rounded-3 break mx-0 mt-4">
                 <div className="col-7 time"><h5 className=''>Break time</h5></div>
-                <div className="col-5 times"><p className='text-muted'>30 seconds</p></div>
+                <div className="col-5 times"><p className='text-muted'><span id='break-time'>0</span> seconds</p></div>
             </div>
         </div>
     );
